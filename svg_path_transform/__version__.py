25a1b855fb78ece771a1b3472bcad69610b2ff77
Igor Mikushkin <igor.mikushkin@gmail.com>
2021-09-09 20:34:18 +0400

    devel version fallback

--- svg_path_transform/__version__.py
+++ svg_path_transform/__version__.py
@@ -1,8 +1,17 @@
 import sys
 
 if sys.version_info < (3, 8):
-    from importlib_metadata import version
+    from importlib_metadata import distribution
 else:
-    from importlib.metadata import version
+    from importlib.metadata import distribution
 
-__version__ = version(__package__)
+_fallback = '(devel)'
+
+try:
+    dist = distribution(__package__)
+    if dist.locate_file('__version__.py') == __file__:
+        __version__ = dist.version
+    else:
+        __version__ = _fallback
+except:
+    __version__ = _fallback
