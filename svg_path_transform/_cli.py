900d082508b9ccd1cf176d8c1bc7e951c8c37290
Igor Mikushkin <igor.mikushkin@gmail.com>
2021-09-09 13:08:08 +0400

    correct prog name on executing the package

--- svg_path_transform/_cli.py
+++ svg_path_transform/_cli.py
@@ -7,7 +7,7 @@ from .__version__ import __version__
 
 
 def _main():
-    parser = argparse.ArgumentParser(description='SVG path data transformer')
+    parser = argparse.ArgumentParser(prog=__package__, description='SVG path data transformer')
     arg = parser.add_argument
     arg('--dx', metavar='N', type=float, default=0., help='translate x by N')
     arg('--dy', metavar='N', type=float, default=0., help='translate y by N')
